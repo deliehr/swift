2a69ad2486b410b595214ebfae2eb66adf3374b0
Dominik Liehr <dominik_liehr@me.com>
2018-07-08 17:00:42 +0200

    fixed range bug
    - changed function signature

--- String+substring.swift
+++ String+substring.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 extension String {
-    func substring(from: Int, to: Int) -> String {
+    func substring(from: Int, inclusiveTo to: Int) -> String {
         let start = self.startIndex
         
         let begin = self.index(start, offsetBy: from)
-        let end = self.index(start, offsetBy: to)
+        let end = self.index(start, offsetBy: (to + 1))
         
         return String(self[begin..<end])
     }
